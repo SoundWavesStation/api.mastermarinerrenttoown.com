f1d5b6e8e0028b331db5bd618a8316a75678ad27
kyamc <kyamc@zylun.com>
2017-09-23 18:51:44 +0800

    Added additional modal class

---
+++ src/MasterMarinerCabRentToOwn.Schema/Sql/TransactionMaster.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MasterMarinerCabRentToOwn.Schema.Sql
+{
+    public class TransactionMaster
+    {
+        public TransactionMaster()
+        {
+            TransactionDetailsMaster = new HashSet<TransactionDetailsMaster>();
+
+        }
+        public int Tmt_TransactionCode { get; set; }
+        public int Tmt_UserCode { get; set; }
+        public UserMaster UserMaster { get; set; }
+        public decimal Tmt_PaymentPerDay { get; set; }
+        public decimal Tmt_TotalQuotation { get; set; }
+        public decimal Tmt_CurrentBalance { get; set; }
+        public decimal Tmt_AccumulatedBalance { get; set; }
+        public string Tmt_TransactionStatus { get; set; }
+
+        public ICollection<TransactionDetailsMaster> TransactionDetailsMaster { get; set; }
+    }
+}
